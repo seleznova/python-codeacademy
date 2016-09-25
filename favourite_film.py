aa34b4abb684d4f8b5566a3a47b1b27575976f5f
Mary Kuzmenko <mary.kuzmenko@gmail.com>
2016-09-25 19:31:36 +0300

    Create favourite_film.py

---
+++ favourite_film.py
@@ -0,0 +1,9 @@
+movie1 = "\n\tA Great Movie"
+
+movie2 = '\n\tAnother Good Movie'
+
+print ('My Favorite Movies:', movie1, movie2)
+
+# This is an exercise with a little bit strange rules since technically correct code does not work. 
+# Because of American and British words "favoUrite" and "favorite"
+# After deleting -U everything is great.
